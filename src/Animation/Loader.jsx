81217e94d4a3401e052318c691ceee0da11f76e3
Marcus Ng <77159089+Marcus0104@users.noreply.github.com>
2024-04-09 22:56:59 +0800

    loader anim delay

--- src/Animation/Loader.jsx
+++ src/Animation/Loader.jsx
@@ -9,19 +9,19 @@ const Loader = () => {
           initial={{ x: "100%", width: "100%" }}
           animate={{ x: "0%", width: "0%" }}
           exit={{ x: ["0%", "100%"], width: ["0%", "100%"] }}
-          transition={{ duration: 0.8, ease: "easeInOut" }}
+          transition={{ duration: 1.0, ease: "easeInOut" }}
         ></motion.div>
         <motion.div
           className="fixed top-0 bottom-0 right-full w-screen h-screen bg-primaryblue z-20"
           initial={{ x: "100%", width: "100%" }}
           animate={{ x: "0%", width: "0%" }}
-          transition={{ delay: 0.5, duration: 0.8, ease: "easeInOut" }}
+          transition={{ delay: 0.7, duration: 0.9, ease: "easeInOut" }}
         ></motion.div>
         <motion.div
           className="fixed top-0 bottom-0 right-full w-screen h-screen bg-dark z-20"
           initial={{ x: "100%", width: "100%" }}
           animate={{ x: "0%", width: "0%" }}
-          transition={{ delay: 0.2, duration: 0.8, ease: "easeInOut" }}
+          transition={{ delay: 0.4, duration: 0.9, ease: "easeInOut" }}
         ></motion.div>
       </div>
     </>
