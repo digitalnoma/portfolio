9f5758afbfb6416b8e75e696e51a409a872cdf1d
digitalnoma <marcusng0104@outlook.com>
2024-04-09 14:20:46 +0800

    tech stack logo

--- src/Components/TechStack/TechStack.jsx
+++ src/Components/TechStack/TechStack.jsx
@@ -5,15 +5,12 @@ import img4 from "../../assets/img4.svg";
 import img5 from "../../assets/img5.svg";
 import img6 from "../../assets/img6.svg";
 import img7 from "../../assets/img7.svg";
-import img8 from "../../assets/img8.svg";
 import img9 from "../../assets/img11.png";
 import img10 from "../../assets/img10.svg";
 import img11 from "../../assets/img11.svg";
-import img12 from "../../assets/img8.png";
 import img13 from "../../assets/img9.png";
 import img14 from "../../assets/img15.svg";
 import img15 from "../../assets/img15.png";
-import img16 from "../../assets/img16.png";
 
 const TechStack = () => {
   const images = [
@@ -24,15 +21,12 @@ const TechStack = () => {
     img5,
     img6,
     img7,
-    img8,
     img9,
     img10,
     img11,
-    img12,
     img13,
     img14,
     img15,
-    img16,
     img1,
     img2,
     img3,
@@ -40,15 +34,12 @@ const TechStack = () => {
     img5,
     img6,
     img7,
-    img8,
     img9,
     img10,
     img11,
-    img12,
     img13,
     img14,
     img15,
-    img16,
   ];
   return (
     <div className=" bg-dark pb-10">
